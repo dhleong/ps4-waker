d952895057ea46f8aee68c5ecb15f84b4720bf90
dhleong <falcone88@gmail.com>
2014-08-06 16:17:29 -0400

    Experimentally support the TCP method in the executable

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var Waker = require('../')
+  , Detector = Waker.Detector
+  , Socket = Waker.Socket
 
   , HOME = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE
   , CREDS_DEFAULT = require('path').join(HOME, '.ps4-wake.credentials.json');
@@ -9,6 +11,7 @@ var argv = require('minimist')(process.argv.slice(2), {
     default: {
         credentials: CREDS_DEFAULT
       , timeout: 5000
+      , pin: ''
     }
   , alias: {
         credentials: 'c'
@@ -35,35 +38,107 @@ if (argv.h || argv.help) {
     console.log('  --device | -d                Specify IP address of a specific PS4');
     console.log('  --failfast                   Don\'t request credentials if none');
     console.log('  --timeout | -t               Timeout in milliseconds');
+    console.log('  --pin <pin-code>             Manual pin-code registration');
     console.log('');
     return;
 }
 
 var waker = new Waker(argv.credentials);
 
-var doWake = function() {
-    waker.wake(argv.timeout, argv.device, function(err) {
+function doWake() {
+    var device = argv.device ? {address: argv.device} : undefined;
+    waker.wake(argv.timeout, device, function(err) {
         if (err) return console.error(err);
 
         console.log("Done!");
     });
 }
 
-waker.on('need-credentials', function() {
+function doRegister(address, creds) {
+
+    var sock = Socket({
+        accountId: creds['user-credential']
+      , host: address
+      , pinCode: argv.pin // if we're already registered, default "" is okay
+    })
+    sock.on('login_result', function(packet) {
+        if (packet.result === 0) {
+            console.log("Logged into device! Future uses should succeed");
+            process.exit(0);
+        } else if (packet.error == "PIN_IS_NEEDED") {
+
+            // prompt the user
+            require('readline').createInterface({
+                input: process.stdin
+              , output: process.stdout
+            }).question("Pin code> ", function(pin) {
+                if (!pin) {
+                    console.error("Pin is required");
+                    process.exist(4);
+                }
+
+                sock.register(pin);
+            });
+
+        } else {
+            console.error("Unexpected error", packet.result, packet.error);
+            process.exit(3);
+        }
+    });
+}
+
+waker.on('need-credentials', function(targetDevice) {
     if (argv.failfast) {
         console.error("No credentials found.");
         process.exit(1);
         return;
     }
+
+    // just assume we need to register as well
+    var address = targetDevice.address;
+    Detector.find(address, argv.timeout, function(err, device) {
+        if (err || device.status != 'OK') {
+            console.error("Device must be awake for initial registration");
+            process.exit(2);
+        }
     
-    console.log("No credentials; Use Playstation App and try to connect to PS4-Waker");
-    waker.requestCredentials(function(err, creds) {
-        if (err) return console.error(err);
-        
-        console.log("Got credentials!", creds);
-        doWake();
+        console.log("No credentials; Use Playstation App and try to connect to PS4-Waker");
+        waker.requestCredentials(function(err, creds) {
+            if (err) return console.error(err);
+            
+            console.log("Got credentials!", creds);
+
+            // okay, now register
+            doRegister(address, creds);
+        });
     });
 });
 
-// do it!
-doWake();
+if (argv.pin) {
+    // find the target machine and register
+    var getCredsAndRegister = function(address) {
+        waker.readCredentials(function(err, creds) {
+            // welllll shit. just fire up the need-credentials workflow
+            if (err) return waker.emit('need-credentials', {address: address});
+
+            doRegister(address, creds);
+        });
+    }
+
+    if (argv.device) {
+        getCredsAndRegister(argv.device);
+    } else {
+        // find any and do it
+        Detector.findAny(argv.timeout, function(err, device, rinfo) {
+            if (err || !rinfo) {
+                console.error("Couldn't find any PS4");
+                process.exit(1);
+            }
+
+            getCredsAndRegister(rinfo.address);
+        });
+    }
+} else {
+    // do it!
+    doWake();
+}
--- index.js
+++ index.js
@@ -2,9 +2,6 @@
 var util = require('util')
   , events = require('events')
   , fs = require('fs')
-  , exec = require('child_process').exec
-  , arp = require('node-arp')
-  , spoof = require('spoof')
   
   , Detector = require('./lib/detector')
   , Dummy = require('./lib/dummy')
@@ -49,9 +46,9 @@ Waker.prototype.wake = function(timeout, device, callback) {
 Waker.prototype._doWake = function(device, callback) {
 
     var self = this;
-    fs.readFile(this.credentials, function(err, buf) {
+    this.readCredentials(function(err, creds) {
         if (err && self.listeners('need-credentials')) {
-            self.emit('need-credentials');
+            self.emit('need-credentials', device);
             return;
         } else if (err) {
             // no listeners? just hop to it
@@ -59,31 +56,35 @@ Waker.prototype._doWake = function(device, callback) {
             return;
         }
 
-        var creds = JSON.parse(buf.toString());
         self.sendWake(device, creds, callback);
     });
 };
+
+Waker.prototype.readCredentials = function(callback) {
+    
+    fs.readFile(this.credentials, function(err, buf) {
+        if (err) return callback(err);
+        
+        callback(JSON.parse(buf.toString()));
+    });
+};
+
 
 Waker.prototype.requestCredentials = function(callback) {
     var self = this;
     var dummy = new Dummy();
     dummy.setStandby();
-    dummy.once('wakeup', function(packet, rinfo) {
+    dummy.once('wakeup', function(packet) {
 
         var creds = CRED_KEYS.reduce(function(data, key) {
             data[key] = packet[key];
             return data;
         }, {});
 
-        arp.getMAC(rinfo.address, function(err, mac) {
+        fs.writeFile(self.credentials, JSON.stringify(creds), function(err) {
             if (err) return callback(err);
 
-            creds.deviceMac = mac;
-            fs.writeFile(self.credentials, JSON.stringify(creds), function(err) {
-                if (err) return callback(err);
-
-                callback(null, creds);
-            });
+            callback(null, creds);
         });
 
         dummy.close();
@@ -95,102 +96,26 @@ Waker.prototype.requestCredentials = function(callback) {
 }
 
 Waker.prototype.sendWake = function(device, creds, callback) {
-    var appMac = creds.deviceMac;
-    delete creds.deviceMac;
 
     // make sure to use standard port
     device.port = ps4lib.DDP_PORT;
 
-    var wifi = spoof.findInterface('Wi-Fi');
-    if (!wifi)
-        return callback(new Error("Could not find Wi-Fi interface"));
+    // send the wake command
+    var udp = ps4lib.udpSocket();
+    udp.bind(function() {
+        udp.setBroadcast(true); // maybe?
 
-    var oldMac = wifi.currentAddress || wifi.address;
-    if (!oldMac)
-        return callback(new Error("SAFETY: Couldn't resolve existing MAC addr"));
+        udp.discover("WAKEUP", creds, device);
 
-    ensureMac(wifi.device, appMac, wifi.port, function(err) {
-        if (err) return callback(err);
-
-        // send the wake command
-        var udp = ps4lib.udpSocket();
-        udp.bind(function() {
-            udp.setBroadcast(true); // maybe?
-
-            udp.discover("WAKEUP", creds, device);
-
-            // TODO actually try to detect it?
-            setTimeout(function() {
-                udp.close();
-
-                // spoof.setInterfaceMAC(wifi.device, oldMac, wifi.port);
-                ensureMac(wifi.device, oldMac, wifi.port, callback);
-
-            }, 1000);
-        });
-
+        // TODO actually try to detect it?
+        setTimeout(function() {
+            udp.close();
+            
+            callback(null);
+        }, 1000);
     });
-}
-
-function getAirportNetwork(deviceName, callback) {
-    
-    exec('networksetup -getairportnetwork ' + deviceName, function(err, stdout) {
-        if (err) return callback(err);
-
-        if (!~stdout.indexOf('Current'))
-            return callback(null, null);
-
-        callback(null, stdout.substr(stdout.indexOf(': ') + 2).trim());
-    });
-}
-
-function powerCycleAirport(deviceName, callback) {
-    var cmd = 'networksetup -setairportpower ' + deviceName + ' ';
-    exec(cmd + 'off', function() {
-        exec(cmd + 'on', function() {
-            setTimeout(callback, 8000);
-        });
-    });
-}
-
-/**
- * Spoof is great, but (at least on my machine) it doesn't
- *  get airport to properly reconnect to the network,
- *  so... let's make sure that happens
- */
-function ensureMac(deviceName, mac, port, callback) {
-    if (spoof.getInterfaceMAC(deviceName) == mac) 
-        return callback();
-
-    if (process.getuid && process.getuid()) {
-        callback(new Error("You must be ROOT to spoof the device's MAC address"));
-        return;
-    }
-
-    try {
-        spoof.setInterfaceMAC(deviceName, mac, port);
-    } catch (e) {
-        return callback(e);
-    }
-
-    if (process.platform == 'darwin') {
-
-        var cycle = function() {
-            getAirportNetwork(deviceName, function(err, network) {
-                // console.log("Got network", err, network);
-                if (err) return callback(err);
-                if (network) return callback();
-
-                // network not up yet!
-                // console.log("No network yet!");
-                powerCycleAirport(deviceName, cycle);
-            });
-        };
-
-        cycle();
-
-        // } else {  // others?
-    }
 }
 
 module.exports = Waker;
+module.exports.Detector = Detector;
+module.exports.Socket = require('./lib/ps4socket');
