4ae9b581a63e0c21045bd361fe0679b50c119de9
Daniel Leong <me@dhleong.net>
2017-05-04 10:24:46 -0400

    Add sendKeys test, and more arg validation on it

--- test/device-test.js
+++ test/device-test.js
@@ -2,7 +2,7 @@
 const events = require('events');
 const EventEmitter = events.EventEmitter;
 const chai = require('chai');
-const Device = require('../').Device;
+const {Device, Socket} = require('../');
 
 const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
@@ -11,29 +11,53 @@ chai.use(chaiAsPromised);
 chai.should();
 
 class FakeSocket extends EventEmitter {
-    constructor() {
+    constructor(device) {
         super();
+        this.isOpen = true;
+        this._device = device;
+
+        this.pendingStandbyResults = [];
     }
 
     close() {
+        this.isOpen = false;
+    }
+
+    remoteControl(op, holdTime) {
+        if (holdTime) {
+            this._device.emit('send_rc_key', op, holdTime);
+        } else {
+            this._device.emit('send_rc_key', op);
+        }
+    }
+
+    requestStandby(cb) {
+        if (!this.pendingStandbyResults.length) {
+            cb(new Error('No pending requestStandby results'));
+            return;
+        }
+
+        var result = this.pendingStandbyResults.shift();
+        cb(result);
     }
 }
 
 class FakeWaker {
     constructor() {
         this.calls = [];
-        this.pendingResult = null;
+        this.pendingResults = [];
     }
 
     wake(opts, device, cb) {
         this.calls.push([opts, device]);
 
-        if (!this.pendingResult) {
+        if (!this.pendingResults.length) {
             throw new Error("No pendingResult set on FakeWaker");
         }
 
-        cb(...this.pendingResult);
-        this.pendingResult = null;
+        let result = this.pendingResults.shift();
+
+        cb(...result);
     }
 }
 
@@ -46,12 +70,17 @@ describe("Device", function() {
     var pendingDetectPromise;
     var waker;
     var socket;
+    var events;
+
+    var _originalSetTimeout;
 
     beforeEach(function() {
         device = new Device();
         waker = new FakeWaker();
-        socket = new FakeSocket();
+        socket = new FakeSocket(device);
+        events = [];
 
+        device._retryDelay = 0;
         device._detect = () => {
             if (pendingDetectPromise) {
                 return pendingDetectPromise.then(d => {
@@ -62,6 +91,23 @@ describe("Device", function() {
             }
         };
         device._waker = () => waker;
+
+        let _deviceDotEmit = device.emit.bind(device);
+        device.emit = function(...args) {
+            _deviceDotEmit(...args);
+            events.push(args);
+        };
+
+        // patch setTimeout so we don't have to wait
+        _originalSetTimeout = global.setTimeout;
+        global.setTimeout = function(cb, delay) {
+            device.emit('*setTimeout', delay);
+            cb();
+        };
+    });
+
+    afterEach(function() {
+        global.setTimeout = _originalSetTimeout;
     });
 
     describe("[test-util]", function() {
@@ -119,7 +165,7 @@ describe("Device", function() {
                 status: 'OK'
             });
 
-            waker.pendingResult = [null, socket];
+            waker.pendingResults.push([null, socket]);
             return device._connectIfAwake().should.become(socket);
         });
     });
@@ -137,7 +183,7 @@ describe("Device", function() {
                 status: 'OK'
             });
 
-            waker.pendingResult = [new Error('wake timeout')];
+            waker.pendingResults.push([new Error('wake timeout')]);
 
             return device.turnOn().should.be.rejectedWith(/wake timeout/);
         });
@@ -148,7 +194,7 @@ describe("Device", function() {
                 status: 'OK'
             });
 
-            waker.pendingResult = [null, socket];
+            waker.pendingResults.push([null, socket]);
 
             return device.turnOn().then(() => {
 
@@ -162,7 +208,151 @@ describe("Device", function() {
     });
 
     describe(".turnOff", function() {
-        // TODO
-        it("retries once");
+        it("does nothing when already off", function() {
+            pendingDetectPromise = Promise.resolve({
+                status: 'Standby'
+            });
+
+            // it would be an error if it tried to connect
+            return device.turnOff().should.become(device);
+        });
+
+        it("retries once", function() {
+            pendingDetectPromise = Promise.resolve({
+                address: '123.456.789.0',
+                status: 'OK'
+            });
+
+            waker.pendingResults.push([null, socket]);
+            waker.pendingResults.push([null, socket]);
+            socket.pendingStandbyResults = ["Error", null];
+
+            return device.turnOff().should.become(device);
+        });
+
+        it("Errors on second error", function() {
+            pendingDetectPromise = Promise.resolve({
+                address: '123.456.789.0',
+                status: 'OK'
+            });
+
+            waker.pendingResults.push([null, socket]);
+            waker.pendingResults.push([null, socket]);
+            socket.pendingStandbyResults = ["Error", "Error2"];
+
+            return device.turnOff().should.be.rejectedWith(/Error2/);
+        });
+    });
+
+    describe(".sendKeys", function() {
+
+        beforeEach(function() {
+            pendingDetectPromise = Promise.resolve({
+                address: '123.456.789.0',
+                status: 'OK'
+            });
+
+            waker.pendingResults.push([null, socket]);
+        });
+
+        it("rejects empty argument", function() {
+            return device.sendKeys().should.be.rejectedWith(/No keys/);
+        });
+
+        it("rejects unknown keys", function() {
+            return device.sendKeys(['awesome-button']).should.be.rejectedWith(/Unknown key names/);
+        });
+
+        it("rejects varargs invocation (single value)", function() {
+            return device.sendKeys('awesome-button')
+                .should.be.rejectedWith(/called with an array/);
+        });
+
+        it("rejects varargs invocation (tuple)", function() {
+            return device.sendKeys(['awesome-button', 2500])
+                .should.be.rejectedWith(/must be a string or a tuple/);
+        });
+
+
+        it("Sends single direction", function() {
+            return device.sendKeys(['right']).then(function() {
+                events.should.deep.equal([
+                    ['*setTimeout', 1500],
+                    ['send_rc_key', Socket.RCKeys.OPEN_RC],
+                    ['*setTimeout', 200],
+
+                    ['send_rc_key', Socket.RCKeys.RIGHT],
+                    ['send_rc_key', Socket.RCKeys.KEY_OFF],
+                    ['sent_key', 'RIGHT'],
+                    ['*setTimeout', 200],
+
+                    ['send_rc_key', Socket.RCKeys.CLOSE_RC],
+                    ['*setTimeout', 200],
+                ]);
+            });
+        });
+
+        it("Holds direction", function() {
+            return device.sendKeys([['left', 1000]]).then(function() {
+                events.should.deep.equal([
+                    ['*setTimeout', 1500],
+                    ['send_rc_key', Socket.RCKeys.OPEN_RC],
+                    ['*setTimeout', 200],
+
+                    // send "down", wait, send "held", then finally clean
+                    ['send_rc_key', Socket.RCKeys.LEFT],
+                    ['*setTimeout', 1000],
+                    ['send_rc_key', Socket.RCKeys.LEFT, 1000],
+                    ['send_rc_key', Socket.RCKeys.KEY_OFF],
+
+                    ['sent_key', 'LEFT'],
+                    ['*setTimeout', 200],
+
+                    ['send_rc_key', Socket.RCKeys.CLOSE_RC],
+                    ['*setTimeout', 200],
+                ]);
+            });
+        });
+
+        it("Sends single ps press", function() {
+            return device.sendKeys(['ps']).then(function() {
+                events.should.deep.equal([
+                    ['*setTimeout', 1500],
+                    ['send_rc_key', Socket.RCKeys.OPEN_RC],
+                    ['*setTimeout', 200],
+
+                    ['send_rc_key', Socket.RCKeys.PS],
+                    ['send_rc_key', Socket.RCKeys.KEY_OFF],
+
+                    ['sent_key', 'PS'],
+                    ['*setTimeout', 1000],
+
+                    ['send_rc_key', Socket.RCKeys.CLOSE_RC],
+                    ['*setTimeout', 200],
+                ]);
+            });
+        });
+
+        it("holds PS button", function() {
+            return device.sendKeys([['ps', 1000]]).then(function() {
+                events.should.deep.equal([
+                    ['*setTimeout', 1500],
+                    ['send_rc_key', Socket.RCKeys.OPEN_RC],
+                    ['*setTimeout', 200],
+
+                    // send "down", wait, send "held"
+                    ['send_rc_key', Socket.RCKeys.PS],
+                    ['*setTimeout', 1000],
+                    ['send_rc_key', Socket.RCKeys.PS, 1000],
+                    // NOTE: we do NOT clear with KEY_OFF
+
+                    ['sent_key', 'PS'],
+                    ['*setTimeout', 1000],
+
+                    ['send_rc_key', Socket.RCKeys.CLOSE_RC],
+                    ['*setTimeout', 200],
+                ]);
+            });
+        });
     });
 });
